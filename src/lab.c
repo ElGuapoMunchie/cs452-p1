2c09e75d19fa00daa24e2d07add1a14e74b8e79d
Mark Muench <122300010+ElGuapoMunchie@users.noreply.github.com>
2024-10-15 22:17:33 +0000

    Decided on structure, todo: Malloc queue array and then update methods accordingly

--- src/lab.c
+++ src/lab.c
@@ -3,3 +3,70 @@ Class:   CS 452 - Operating Systems
 Project: P2 (aka P6) Bounded Buffer
 Author:  Mark Muench
 */
+
+#include "lab.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+queue_t queue_init(int capacity)
+{
+    queue_t queue;
+
+    // TODO: Do I need to malloc queue? -- Unsure.
+
+    queue->capacity = capacity;
+    queue->currSize = 0;
+    queue->shutdown = false;
+
+    // Malloc array based on new capacity.
+
+    return queue;
+}
+
+void queue_destroy(queue_t q);
+
+void enqueue(queue_t q, void *data)
+{
+    printf("%p\n", data);
+    
+    q->currSize++; // Incremement number of items in queue
+}
+
+void *dequeue(queue_t q)
+{
+
+    // Return if queue is empty
+    if (q->currSize == 0)
+    {
+        return;
+    }
+
+    Node *retVal = &q->node; // Grab current node address
+    q->node = *q->node.next; // Set currNode to nextNode
+    q->currSize--;           // Decrement number of items in queue
+
+    return retVal;           // Return item at address
+}
+
+void queue_shutdown(queue_t q)
+{
+    q->shutdown = true;
+}
+
+bool is_empty(queue_t q)
+{
+    if (q->currSize == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool is_shutdown(queue_t q)
+{
+    return q->shutdown;
+}
--- src/lab.h
+++ src/lab.h
@@ -14,62 +14,71 @@ extern "C"
 {
 #endif
 
-    /**
-     * @brief opaque type definition for a queue
-     */
-    typedef struct queue *queue_t;
 
-    /**
-     * @brief Initialize a new queue
-     *
-     * @param capacity the maximum capacity of the queue
-     * @return A fully initialized queue
-     */
-    queue_t queue_init(int capacity);
+  /**
+   * @brief opaque type definition for a queue
+   */
+  typedef struct queue
+  {
+    int capacity;  // Capacity of queue
+    int currSize;  // Current Size
+    bool shutdown; // Flag to shutdown. If flag is set, only consumers can access queue.
+    void** array;  // Array of pointers to data stored in the queue
+    int head;      // int used to access head of items in queue
+    int tail;      // int used to access tail of items in queue
+  } *queue_t;
 
-    /**
-     * @brief Frees all memory and related data signals all waiting threads.
-     *
-     * @param q a queue to free
-     */
-    void queue_destroy(queue_t q);
+  /**
+   * @brief Initialize a new queue
+   *
+   * @param capacity the maximum capacity of the queue
+   * @return A fully initialized queue
+   */
+  queue_t queue_init(int capacity);
 
-    /**
-     * @brief Adds an element to the back of the queue
-     *
-     * @param q the queue
-     * @param data the data to add
-     */
-    void enqueue(queue_t q, void *data);
+  /**
+   * @brief Frees all memory and related data signals all waiting threads.
+   *
+   * @param q a queue to free
+   */
+  void queue_destroy(queue_t q);
 
-    /**
-     * @brief Removes the first element in the queue.
-     *
-     * @param q the queue
-     */
-    void *dequeue(queue_t q);
+  /**
+   * @brief Adds an element to the back of the queue
+   *
+   * @param q the queue
+   * @param data the data to add
+   */
+  void enqueue(queue_t q, void *data);
 
-    /**
-     * @brief Set the shutdown flag in the queue so all threads can
-     * complete and exit properly
-     *
-     * @param q The queue
-     */
-   void queue_shutdown(queue_t q);
+  /**
+   * @brief Removes the first element in the queue.
+   *
+   * @param q the queue
+   */
+  void *dequeue(queue_t q);
 
-    /**
-     * @brief Returns true is the queue is empty
-     *
-     * @param q the queue
-     */
-    bool is_empty(queue_t q);
+  /**
+   * @brief Set the shutdown flag in the queue so all threads can
+   * complete and exit properly
+   *
+   * @param q The queue
+   */
+  void queue_shutdown(queue_t q);
 
-    /**
-     * @brief
-     *
-     * @param q The queue
-     */
-    bool is_shutdown(queue_t q);
+  /**
+   * @brief Returns true is the queue is empty
+   *
+   * @param q the queue
+   */
+  bool is_empty(queue_t q);
+
+  /**
+   * @brief
+   *
+   * @param q The queue
+   */
+  bool is_shutdown(queue_t q);
 
 #ifdef __cplusplus
 } // extern "C"
